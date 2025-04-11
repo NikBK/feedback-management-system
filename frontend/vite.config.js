5fc9de90e274c8290d55ff3c18200ab166ae7a0c
Nikhil BK <67317835+NikBK@users.noreply.github.com>
2025-04-11 11:10:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Vite’s default, but you can customize it
+    outDir: 'dist',
   }
 })
