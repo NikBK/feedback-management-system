20d38fe946e41676bb59481cd2db873611baf239
Nikhil BK <67317835+NikBK@users.noreply.github.com>
2025-04-11 13:37:16 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,11 @@ const { notFound, errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://nikbk-feedback-management-system.vercel.app',
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/auth', authRoutes);
